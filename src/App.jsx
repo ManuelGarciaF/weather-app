f554d71adbe9aab843712256f29d9cc30d0e4385
Manuel Garcia F <mgarciafrigo@gmail.com>
2020-02-03 15:32:35 -0300

    install node-sass and eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import Axios from 'axios';
 
-function App() {
-  return <div className="App">Hello World</div>;
+const API_KEY = '09c4d436f979cacc5cd4e3dd944cefb2';
+
+async function fetchWeatherData(query) {
+  const response = await Axios.get(
+    `api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}`,
+  );
+  const data = await response.json();
+  console.log(data);
+}
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <button type="button" onClick={() => fetchWeatherData('Gualeguaychu,AR')}>
+          asdfdas
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
