08c729be6f3a2b7bde4b17f0f0117bb8fab567f4
Manuel Garcia F <mgarciafrigo@gmail.com>
2020-03-01 12:22:47 -0300

    Updates

---
+++ precache-manifest.32f5c3390915287398f830e11acbf43c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84280325119721c3ae99f02a7bd6078e",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "c83c1406cf5dde236012",
+    "url": "/weather-app/static/css/main.3cd93f74.chunk.css"
+  },
+  {
+    "revision": "eb871252d6cc3a42395b",
+    "url": "/weather-app/static/js/2.d8f7b08f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather-app/static/js/2.d8f7b08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83c1406cf5dde236012",
+    "url": "/weather-app/static/js/main.1cad03e8.chunk.js"
+  },
+  {
+    "revision": "48f3787e04d45de6cbd1",
+    "url": "/weather-app/static/js/runtime-main.36d91d67.js"
+  },
+  {
+    "revision": "4759b6a937f232aec2850c9134563ee4",
+    "url": "/weather-app/static/media/clear.4759b6a9.jpg"
+  },
+  {
+    "revision": "048cec8505b4c8a8e4c17f7132de1297",
+    "url": "/weather-app/static/media/clouds.048cec85.jpg"
+  },
+  {
+    "revision": "9fcc6259fbf5236b9b7dc2bfc8a81e53",
+    "url": "/weather-app/static/media/mist.9fcc6259.jpg"
+  },
+  {
+    "revision": "c94d0ecb0857632749feaa979aa263b9",
+    "url": "/weather-app/static/media/rain.c94d0ecb.jpg"
+  },
+  {
+    "revision": "4266641e730b1ae0d65dfec862a2c3fd",
+    "url": "/weather-app/static/media/snow.4266641e.jpg"
+  },
+  {
+    "revision": "e77e9a8f39969267791c98bac035ee32",
+    "url": "/weather-app/static/media/thunderstorm.e77e9a8f.jpg"
+  }
+]);
